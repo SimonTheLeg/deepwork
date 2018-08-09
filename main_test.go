92e5f4c90523a0d17780e8ac66879b3319d56381
Simon Bein <sbein@thatisnomoon.io>
2018-08-09 21:50:28 +0200

    Add unit test for parseConfig for directory perms 000, 100, 200, 300

---
+++ main_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"testing"
+
+	homedir "github.com/mitchellh/go-homedir"
+)
+
+var UserHomeDir string
+
+func init() {
+	// Determine Users home directory
+	var err error
+	UserHomeDir, err = homedir.Dir()
+	if err != nil {
+		log.Fatalf("Could not determine Users home directory: %v", err)
+	}
+}
+
+func TestParseConfig(t *testing.T) {
+	difFolderPerm := []struct {
+		name           string
+		folderPerm     os.FileMode
+		configLocation string
+		expResp        string
+	}{
+		{
+			"no perm, directory exists",
+			0000,
+			"/tmp/noPermissionsHere/config.json",
+			"Could not access config file: open /tmp/noPermissionsHere/config.json: permission denied",
+		},
+		{
+			"execute only, directory doesn't exist yet",
+			0100,
+			"/tmp/executeOnly/config.json",
+			"Could not write default config: open /tmp/executeOnly/config.json: permission denied",
+		},
+		{
+			"write only",
+			0200,
+			"/tmp/writeOnly/config.json",
+			"Could not access config file: open /tmp/writeOnly/config.json: permission denied",
+		},
+		{
+			"write & execute",
+			0300,
+			"/tmp/writeAndExecute/config.json",
+			"no error",
+		},
+	}
+
+	for _, tc := range difFolderPerm {
+		t.Run(tc.name, func(t *testing.T) {
+			confDir := path.Dir(tc.configLocation)
+
+			if err := os.Mkdir(confDir, tc.folderPerm); err != nil {
+				t.Fatalf("Could not create dir: %s: %v", confDir, err)
+			}
+
+			_, err := parseConfig(tc.configLocation)
+
+			// Check if the response from parseConfig, is the expected one
+			if err != nil {
+				if err.Error() != tc.expResp {
+					t.Errorf("Expected: '%v' got: '%v'", tc.expResp, err.Error())
+				}
+			} else {
+				if tc.expResp != "no error" {
+					t.Errorf("Got no error, but expected '%v'", tc.expResp)
+				}
+			}
+
+			// Make sure in the successful cases the file is actually there
+			if tc.expResp == "no error" {
+				cont, err := ioutil.ReadFile(tc.configLocation)
+				if err != nil {
+					t.Errorf("Expected a file at %s, but could not read it due to: %v", tc.configLocation, err)
+				}
+				defaultConfig := []byte(`{"affectedApps":["Mail","Calendar"]}`)
+				if bytes.Compare(cont, defaultConfig) != 0 {
+					t.Errorf("%s should have the following content: '%v', but has '%v'", tc.configLocation, defaultConfig, cont)
+				}
+			}
+
+			// Clean up
+			os.Chmod(confDir, 0700)
+			if err := os.RemoveAll(confDir); err != nil {
+				t.Fatalf("Could not clean up '%s': %v; Please clean up this directory manually, as it can corrupt further testing", confDir, err)
+			}
+		})
+	}
+}
