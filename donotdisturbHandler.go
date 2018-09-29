2d9974a8e6ad5c1a2b5796909f225529bbe8b1d0
Simon Bein <simontheleg@gmail.com>
2018-09-29 13:09:26 +0200

    Add Method for enabling Do Not Disturb

--- donotdisturbHandler.go
+++ donotdisturbHandler.go
@@ -3,25 +3,51 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
-func isNotificationCenterOn() (bool, error) {
-	cmd := exec.Command("defaults", "-currentHost", "read", "~/Library/Preferences/ByHost/com.apple.notificationcenterui", "doNotDisturb")
+func restartNotificationCenter() error {
+	cmd := exec.Command("bash", "-c", "killall NotificationCenter")
 
-	output, err := cmd.Output()
+	_, err := cmd.Output()
 
 	if err != nil {
-		return false, fmt.Errorf("Could not determine Notification Center State; %v", err)
+		return err
 	}
 
-	outputstr := string(output)
+	return nil
+}
 
-	switch outputstr {
-	case "0": // Aka Notification Center is off
-		return false, nil
-	case "1": // Aka Notification Center is on
-		return true, nil
+// TurnDoNotDisturbOn turns on Do Not Disturb on Mac OS X
+func TurnDoNotDisturbOn() error {
+
+	// Enable Do Not Disturb Mode
+	cmd := exec.Command("bash", "-c", "defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturb -boolean true")
+
+	_, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return err
 	}
 
-	return false, fmt.Errorf("Could not determine Notification Center State: An unexpected error occurred")
+	// Set Time forDo Not Disturb Mode
+	t := time.Now()
+	tfmt := fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d +0000", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+
+	cmd = exec.Command("bash", "-c", fmt.Sprintf("defaults -currentHost write ~/Library/Preferences/ByHost/com.apple.notificationcenterui doNotDisturbDate -date '%s'", tfmt))
+
+	_, err = cmd.CombinedOutput()
+
+	if err != nil {
+		return err
+	}
+
+	// Restart Notification Center
+	err = restartNotificationCenter()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
